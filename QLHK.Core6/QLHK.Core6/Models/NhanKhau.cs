531839db2d92969884332bf628b00c58fca932fe
Vishagna <111635138+silasblade@users.noreply.github.com>
2023-12-09 02:47:39 +0700

    Update

---
+++ QLHK.Core6/QLHK.Core6/Models/NhanKhau.cs
@@ -0,0 +1,19 @@
+﻿namespace QLHK.Models
+{
+    public class NhanKhau
+    {
+        public string MaNhanKhau { get; set; }
+        public string MaHoKhau { get; set; }
+        public string HoVaTen { get; set; }
+        public string GioiTinh { get; set; }
+        public int NamSinh { get; set; }
+        public string QHVoiChuHo { get; set; }
+        public string NoiSinh { get; set; }
+        public string QueQuan { get; set; }
+        public string DanToc { get; set; }
+        public string NgheNghiep { get; set; }
+        public string NoiLamViec { get; set; }
+        public string CCCD { get; set; }
+    }
+
+}
